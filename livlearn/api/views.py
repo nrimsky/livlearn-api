6f850169baa392ce82e49a44dc126d52bc3ce438
Nina <ninarimsky@gmail.com>
2021-06-10 19:03:40 +0100

    comma seperated filters

--- livlearn/api/views.py
+++ livlearn/api/views.py
@@ -2,16 +2,35 @@ from livlearn.api.models import Link, Tag
 from livlearn.api.serializers import LinkSerializer, TagSerializer
 from rest_framework import generics
 from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, MultipleChoiceFilter, CharFilter, FilterSet, ModelMultipleChoiceFilter
+from django_filters.fields import CSVWidget
+
+
+class LinkFilter(FilterSet):
+
+    name = CharFilter(lookup_expr='icontains')
+    description = CharFilter(lookup_expr='icontains')
+    tagline = CharFilter(lookup_expr='icontains')
+    tags = ModelMultipleChoiceFilter(queryset=Tag.objects.all(), widget=CSVWidget())
+    type = MultipleChoiceFilter(
+        distinct=True, widget=CSVWidget(), choices=Link.TYPE_CHOICES
+    )
+    level = MultipleChoiceFilter(
+        distinct=True, widget=CSVWidget(), choices=Link.LEVEL_CHOICES
+    )
+
+    class Meta:
+        model = Link
+        fields = ['tags', 'type', 'level', 'name', 'description', 'tagline']
 
 
 class LinkListView(generics.ListAPIView):
     serializer_class = LinkSerializer
     queryset = Link.objects.all()
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = LinkFilter
     search_fields = ['name', 'description', 'tagline', 'tags']
     ordering_fields = ['created_at']
-    filterset_fields = '__all__'
     ordering = ['created_at']
 
 
