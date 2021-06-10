dca94a21c071884f66ebc9cafd31568d629b62ea
Nina <ninarimsky@gmail.com>
2021-06-10 15:57:15 +0100

    add more filtering and sorting + disallow post

--- livlearn/api/views.py
+++ livlearn/api/views.py
@@ -1,16 +1,24 @@
 from livlearn.api.models import Link, Tag
 from livlearn.api.serializers import LinkSerializer, TagSerializer
 from rest_framework import generics
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 
-class LinkListView(generics.ListCreateAPIView):
+class LinkListView(generics.ListAPIView):
     serializer_class = LinkSerializer
     queryset = Link.objects.all()
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description', 'tagline', 'tags']
+    ordering_fields = ['created_at']
     filterset_fields = '__all__'
+    ordering = ['created_at']
 
 
-class TagListView(generics.ListCreateAPIView):
+class TagListView(generics.ListAPIView):
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    ordering = ['name']
+    ordering_fields = ['name']
+    search_fields = ['name']
--- livlearn/urls.py
+++ livlearn/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from django.contrib import admin
 from livlearn.api import views
 
